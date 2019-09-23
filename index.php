f3fcef4ccb421d3a0d580e494a162cf0a8d16cc0
mgallagher56 <marc.gallagherreid11@bathspa.ac.uk>
2019-09-23 14:33:34 +0100

    changed connection to correct db

--- index.php
+++ index.php
@@ -7,8 +7,9 @@
  * @return PDO db connection
  */
 function connectDb() {
-    $db = new PDO("mysql:host=db; dbname=accounts","root","password");
+    $db = new PDO("mysql:host=db; dbname=marcCollection","root","password");
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
     return $db;
 }
 $db = connectDb();
+
