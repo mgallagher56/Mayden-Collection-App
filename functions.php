f579f34dafa5fb027b4d7b17ff85d81c1dca1f62
mgallagher56 <marc.gallagherreid11@bathspa.ac.uk>
2019-09-23 15:11:06 +0100

    created function to extract data from db as array

--- functions.php
+++ functions.php
@@ -10,3 +10,13 @@ function connectDb() {
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
     return $db;
 }
+
+
+
+function getdataFromDb($db)
+{
+    $query = $db->prepare("SELECT `name`, `brand`, `primary colour`, `release year` FROM `Shoes`");
+    $query->execute();
+    $collectionArr = $query->fetchall();
+    return $collectionArr;
+}
--- index.php
+++ index.php
@@ -4,3 +4,7 @@ require_once 'functions.php';
 
 $db = connectDb();
 
+$collectionArr = getdataFromDb($db);
+
+var_dump($collectionArr);
+
