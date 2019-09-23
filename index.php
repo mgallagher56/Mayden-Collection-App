a8c015eab0b592629bbaff1c46e5638a6336eeb6
mgallagher56 <marc.gallagherreid11@bathspa.ac.uk>
2019-09-23 14:31:12 +0100

    created index.php file with db connection function

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+/**
+ * Connects to mySQL database
+ *
+ * @return PDO db connection
+ */
+function connectDb() {
+    $db = new PDO("mysql:host=db; dbname=accounts","root","password");
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+    return $db;
+}
+$db = connectDb();
