3fa5f3fc5bfe6ed91b0b3448c83fdf9bad4e5f8b
mgallagher56 <marc.gallagherreid11@bathspa.ac.uk>
2019-09-25 16:38:57 +0100

    added binding parameters and validation

--- input.php
+++ input.php
@@ -6,6 +6,11 @@ $brand = $_POST['brand'];
 $colour = $_POST['colour'];
 $year = $_POST['year'];
 
-$query = $db->prepare("INSERT INTO Shoes (name, brand, primary_colour, release_year) VALUES ('$name', '$brand','$colour','$year');");
+$query = $db->prepare("INSERT INTO Shoes (name, brand, primary_colour, release_year) VALUES (:names, :brand, :colour, :year);");
+$query->bindParam(':names', $name, PDO::PARAM_STR, 255);
+$query->bindParam(':brand', $brand, PDO::PARAM_STR, 255);
+$query->bindParam(':colour', $colour, PDO::PARAM_STR, 255);
+$query->bindParam(':year', $year, PDO::PARAM_INT,4);
 $query->execute();
+
 header("Location: index.php");
