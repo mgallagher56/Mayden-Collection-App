193043ce4e9c6f3fbcd31506e5f76a9db84801d1
mgallagher56 <marc.gallagherreid11@bathspa.ac.uk>
2019-09-26 17:01:52 +0100

    added colour and hover effects

--- index.php
+++ index.php
@@ -13,15 +13,17 @@ $results = outputDataAsRows($collectionData);
     <link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
 <body>
-<h1>Marc's SuperFun Shoe Collection</h1>
-<div>
-    <ul class="header">
-        <li class="heading">Name</li>
-        <li class="heading">Brand</li>
-        <li class="heading">Primary Colour</li>
-        <li class="heading">Release Year</li>
-        <li class="heading">Image</li>
-    </ul>
+<div class="container">
+    <h1>Marc's SuperFun Shoe Collection</h1>
+    <div>
+        <ul class="header">
+            <li class="heading">Name</li>
+            <li class="heading">Brand</li>
+            <li class="heading">Primary Colour</li>
+            <li class="heading">Release Year</li>
+            <li class="heading">Image</li>
+        </ul>
+    </div>
 </div>
 
 <div>
@@ -30,11 +32,11 @@ $results = outputDataAsRows($collectionData);
 
 <form action="input.php?auth=true" method="post">
     <h3>New Entry</h3>
-    Enter Shoe Name: <input class="textBox" type="text" name="name" placeholder="e.g. Yeezy Boost" required>
-    Enter Brand: <input class="textBox" type="text" name="brand" placeholder="e.g. adidas" required>
-    Enter Primary Colour: <input class="textBox" type="text" name="colour" placeholder="e.g. black" required>
-    Enter Year of Release: <input class="textBox" type="text" name="year" placeholder="e.g. 1999" required>
-    Enter Image Url: <input class="textBox" type="text" name="image" placeholder="e.g. www.flightclub.com/shoe.jpg" required>
+    <p>Enter Shoe Name: <input max="255" class="textBox" type="text" name="name" placeholder="e.g. Yeezy Boost" required></p>
+    <p>Enter Brand: <input max="255" class="textBox" type="text" name="brand" placeholder="e.g. adidas" required></p>
+    <p>Enter Primary Colour: <input max="50" class="textBox" type="text" name="colour" placeholder="e.g. black" required></p>
+    <p>Enter Year of Release: <input min="4" max="4" class="textBox" type="text" name="year" placeholder="e.g. 1999" required></p>
+    <p>Enter Image Url: <input class="textBox" type="url" name="image" placeholder="e.g. www.flightclub.com/shoe.jpg" required></p>
     <input class="submit" type="submit">
 </form>
 
