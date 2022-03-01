2e704bba5a0a1a4ab1651d00be342b626ad7034c
Marc <marc.gallagherreid11@bathspa.ac.uk>
2022-03-01 18:54:38 +0000

    add port

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
  * @return PDO db connection
  */
 function connectDb(): PDO {
-    $db = new PDO('"mysql:host=remotemysql.com; dbname=dBNKXd2Q3o","dBNKXd2Q3o","kB9Qz9f9iv"');
+    $db = new PDO('"mysql:host=remotemysql.com:3306; dbname=dBNKXd2Q3o","dBNKXd2Q3o","kB9Qz9f9iv"');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
     return $db;
 }
